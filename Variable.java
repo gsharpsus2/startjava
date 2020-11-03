3a2452f83d9005f9ba605a32e7cb2f48317ac8f3
gsharpsus2 <gsharpsus2@gmail.com>
2020-11-03 11:19:49 +0300

    Новая программа: Variable

---
+++ Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+	public static void main(String[] args) {
+		byte b=12;
+		short s=2070;
+		int i=8192;
+		long l=10800;
+		float f=103.7f;
+		double d=233.3d;
+		char c='A';
+		boolean bo=true;
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(bo);
+	}
+}
